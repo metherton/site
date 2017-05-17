b9d14e00f01de6eeb3e608572e1e20cd536a37d1
metherton <martinetherton63@gmail.com>
2017-05-17 22:26:44 +0200

    permit / path

--- src/main/java/com/martinetherton/site/SiteApplication.java
+++ src/main/java/com/martinetherton/site/SiteApplication.java
@@ -2,9 +2,10 @@ package com.martinetherton.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class SiteApplication {
+public class SiteApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiteApplication.class, args);
---
+++ src/main/java/com/martinetherton/site/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.martinetherton.site;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+/**
+ * Created by martin on 17/05/17.
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity
+                .ignoring()
+                .antMatchers("/resources/**");
+
+
+//        webSecurity
+//                .ignoring()
+//                // All of Spring Security will ignore the requests
+//                .antMatchers(HttpMethod.POST, "/assignment_callback.json")
+//        ;
+
+//        webSecurity
+//                .ignoring()
+//                // All of Spring Security will ignore the requests
+//                .antMatchers(HttpMethod.POST, "/enqueue_call")
+//        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/signup","/about", "/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+}
